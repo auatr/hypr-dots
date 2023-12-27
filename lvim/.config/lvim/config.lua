ab2eaddf8208a9cf4cc4fec0f8a263542c72a7ab
Cameron McLaughlin <cronmclaughlin@gmail.com>
2023-12-27 08:38:18 -0500

    Remove arrow keys in Lunarvim

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -3,7 +3,8 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 lvim.plugins = {
-  {"folke/tokyonight.nvim"}
+  {"folke/tokyonight.nvim"},
+  {"vimwiki/vimwiki"}
 }
 
 
@@ -11,6 +12,12 @@ require("tokyonight").setup({
   style = "moon",
   transparent = true
 })
-
 lvim.colorscheme = "tokyonight"
 
+local keymap = vim.api.nvim_set_keymap
+
+local opts = { noremap = true, silent = true }
+keymap('n', '<Up>', '<Nop>', opts)
+keymap('n', '<Down>', '<Nop>', opts)
+keymap('n', '<Left>', '<Nop>', opts)
+keymap('n', '<Right>', '<Nop>', opts)
